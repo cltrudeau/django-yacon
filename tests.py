2b9ab254487d75abbbb992ae52dd86cc6532b56f
Christopher Trudeau <ctrudeau@athena.arsensa.info>
2011-03-21 11:08:09 -0400

    - moved group code into yacon from separate package

--- tests.py
+++ tests.py
@@ -83,6 +83,7 @@ class ContentHierarchyTestCase(unittest.TestCase):
         string = grandchild2.node_to_path()
         self.assertEquals(test_string, string)
 
+
 # ============================================================================
 # Page Test Cases
 # ============================================================================
@@ -114,3 +115,124 @@ class PageTestCase(unittest.TestCase):
         self.assertEquals(len(pages), 2)
         self.assertTrue(self.steak in pages)
         self.assertTrue(self.smoking in pages)
+
+
+# ============================================================================
+# GroupOfGroups Test Cases
+# ============================================================================
+
+from django.contrib.auth.models import User, Group
+from yacon.models.groupsq import GroupOfGroups, OwnedGroupOfGroups
+
+class GroupOfGroupsTestCase(unittest.TestCase):
+    def _user(self, username, email, password):
+        try:
+            u = User.objects.get(username=username)
+        except User.DoesNotExist:
+            u = User.objects.create_user(username, email, password)
+
+        return u
+
+    def _group(self, name, users):
+        try:
+            g = Group.objects.get(name=name)
+        except Group.DoesNotExist:
+            g = Group(name=name)
+            g.save()
+
+            for user in users:
+                user.groups.add(g)
+                user.save()
+
+        return g
+
+    def setUp(self):
+        # create some users and groups for testing
+        self.homer = self._user('homer', 'homer@powerplant.com', 'password')
+        self.lenny = self._user('lenny', 'lenny@powerplant.com', 'password')
+        self.carl = self._user('carl', 'carl@powerplant.com', 'password')
+        self.waylon = self._user('waylon', 'waylon@powerplant.com', 'password')
+        self.montgomery = self._user('montgomery', 'montgomery@powerplant.com', 
+            'password')
+        self.hans = self._user('hans', 'hans@powerplant.com', 'password')
+        self.fritz = self._user('fritz', 'fritz@powerplant.com', 'password')
+
+        users = [self.homer, self.lenny, self.carl]
+        self.powerplant_peons = self._group('powerplant_peons', users)
+
+    def test_gog(self):
+        # create a new gog and put a user in it
+        powerplant_owners = GroupOfGroups(name='powerplant_owners')
+        powerplant_owners.save()
+        powerplant_owners.add(self.montgomery)
+
+        # test the has_user functionality
+        result = powerplant_owners.has_user(self.montgomery)
+        self.assertTrue(result)
+
+        # test the list user functionality
+        users = powerplant_owners.list_users()
+        self.assertEquals(len(users), 1)
+        self.assertTrue(self.montgomery in users)
+
+        # create a new gog and add it as a subrelation
+        germans = GroupOfGroups(name='germans')
+        germans.save()
+        germans.add(self.hans)
+        germans.add(self.fritz)
+
+        powerplant_owners.add(germans)
+
+        # test the has_user functionality for the nested group
+        result = powerplant_owners.has_user(self.hans)
+        self.assertTrue(result)
+
+        # test the list user functionality
+        users = powerplant_owners.list_users()
+        self.assertEquals(len(users), 3)
+        self.assertTrue(self.montgomery in users)
+        self.assertTrue(self.hans in users)
+        self.assertTrue(self.fritz in users)
+
+        # create a new gog that has a user, a group and a nested gog
+        powerplant_employees = GroupOfGroups(name='powerplant_employees')
+        powerplant_employees.save()
+        powerplant_employees.add(self.waylon)
+        powerplant_employees.add(powerplant_owners)
+        powerplant_employees.add(self.powerplant_peons)
+
+        # test the has_user functionality for each type of item
+        result = powerplant_employees.has_user(self.waylon)
+        self.assertTrue(result)
+        result = powerplant_employees.has_user(self.hans)
+        self.assertTrue(result)
+        result = powerplant_employees.has_user(self.homer)
+        self.assertTrue(result)
+
+        # test the list user functionality
+        users = powerplant_employees.list_users()
+        self.assertEquals(len(users), 7)
+        self.assertTrue(self.montgomery in users)
+        self.assertTrue(self.hans in users)
+        self.assertTrue(self.fritz in users)
+        self.assertTrue(self.waylon in users)
+        self.assertTrue(self.homer in users)
+        self.assertTrue(self.lenny in users)
+        self.assertTrue(self.carl in users)
+
+    def test_owned_group(self):
+        homer_friends = OwnedGroupOfGroups(owner=self.homer, name='friends')
+        homer_friends.save()
+        homer_friends.add(self.lenny)
+        homer_friends.add(self.carl)
+
+        hans_friends = OwnedGroupOfGroups(owner=self.hans, name='friends')
+        hans_friends.save()
+        hans_friends.add(self.fritz)
+
+        test = OwnedGroupOfGroups.objects.filter(owner=self.homer, 
+            name='friends')
+        friends = test.list_users()
+        self.assertEquals(len(users), 2)
+        self.assertTrue(self.lenny in users)
+        self.assertTrue(self.carl in users)
