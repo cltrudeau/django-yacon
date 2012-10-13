15f2c1cc9c586b9f39ec479c96ee0c257f222838
Web 345 <web345@webfaction.com>
2012-10-13 18:38:32 +0000

    - added "src" as allowed bleach "img" attr

--- definitions.py
+++ definitions.py
@@ -17,6 +17,7 @@ ALLOWED_TAGS = [
     'code',
     'em',
     'img',
+    'i',
     'li',
     'ol',
     'strong',
@@ -29,4 +30,5 @@ ALLOWED_TAGS = [
 
 ALLOWED_ATTRIBUTES = {
     'a' : ['href', 'title'],
+    'img' : ['src', ],
 }
